90d41d77becbd171b21510a23b27bd900caf79d7
vickhoff <dvickhoff@gmail.com>
2026-02-22 10:24:35 +0100

    Fix med arrays and add bronze section

---
+++ data/medalData.js
@@ -0,0 +1,72 @@
+export const goldMedals = [
+    {
+        sport: "Skiathlon 7.5+7.5 km",
+        medalist: ["Frida Karlsson"]
+    },
+    {
+        sport: "Sprint Classic",
+        medalist: ["Linn Svahn"]
+    },
+    {
+        sport: "10km Interval Start Freestyle",
+        medalist: ["Frida Karlsson"]
+    },
+    {
+        sport: "Women's Sprint Freestyle",
+        medalist: ["Jonna Sundling", "Maja Dahlqvist"]
+    },
+    {
+        sport: "Biathlon - Men's 12.5km Pursuit",
+        medalist: ["Martin Ponsiluoma"]
+    },
+    {
+        sport: "Curling - Mixed Doubles",
+        medalist: ["Isabella Wranå", "Rasmus Wranå"]
+    }
+]
+
+export const silverMedals = [
+    {
+        sport: "Women's 10+10km Skiathlon",
+        medalist: ["Ebba Andersson"]
+    },
+    {
+        sport: "Women's Sprint Classic",
+        medalist: ["Jonna Sundling"]
+    },
+    {
+        sport: "Women's 10km Interval Start Freestyle",
+        medalist: ["Ebba Andersson"]
+    },
+    {
+        sport: "Women's 4x7.5km Relay",
+        medalist: ["Linn Svahn", "Ebba Andersson", "Frida Karlsson", "Jonna Sundling"]
+    },
+    {
+        sport: "Women's Giant Slalom",
+        medalist: ["Sara Hector"]
+    },
+    {
+        sport: "Women's Biathlon 4 x 6km Relay",
+        medalist: ["Linn Gestblom","Anna Magnusson", "Elvira Öberg", "Hanna Öberg"]
+    }
+]
+
+export const bronzeMedals = [
+    {
+        sport: "Cross-Country Skiing - Women's Sprint Classic",
+        medalist: ["Maja Dahlqvist"]
+    },
+    {
+        sport: "Alpine Skiing - Women's Slalom",
+        medalist: ["Anna Swenn-Larsson"]
+    },
+    {
+        sport: "Freestyle Skiing - Women's Ski Cross",
+        medalist: ["Sandra Näslund"]
+    },
+    {
+        sport: "Biathlon - Men's 4x7.5km Relay",
+        medalist: ["Viktor Brandt", "Jesper Nelin", "Martin Ponsiluoma", "Sebastian Samuelsson"]
+    }
+]
