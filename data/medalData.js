98c6f79bb83223f207ff1aa9fee33e8ff85a86d4
vickhoff <dvickhoff@gmail.com>
2026-02-22 11:50:40 +0100

    add reset css

--- data/medalData.js
+++ data/medalData.js
@@ -1,72 +1,88 @@
 export const goldMedals = [
     {
         sport: "Skiathlon 7.5+7.5 km",
-        medalist: ["Frida Karlsson"]
+        medalist: ["Frida Karlsson"],
+        image: "./images/medalists/frida-karlsson.png"
     },
     {
         sport: "Sprint Classic",
-        medalist: ["Linn Svahn"]
+        medalist: ["Linn Svahn"],
+        image: "./images/medalists/linn-svahn.png"
     },
     {
         sport: "10km Interval Start Freestyle",
-        medalist: ["Frida Karlsson"]
+        medalist: ["Frida Karlsson"],
+        image: "./images/medalists/frida-karlsson2.png"
     },
     {
         sport: "Women's Sprint Freestyle",
-        medalist: ["Jonna Sundling", "Maja Dahlqvist"]
+        medalist: ["Jonna Sundling", "Maja Dahlqvist"],
+        image: "./images/medalists/jonna-maja.png"
     },
     {
         sport: "Biathlon - Men's 12.5km Pursuit",
-        medalist: ["Martin Ponsiluoma"]
+        medalist: ["Martin Ponsiluoma"],
+        image: "./images/medalists/martin-ponsiluoma.png"
     },
     {
         sport: "Curling - Mixed Doubles",
-        medalist: ["Isabella Wranå", "Rasmus Wranå"]
+        medalist: ["Isabella Wranå", "Rasmus Wranå"],
+        image: "./images/medalists/isabella-rasmus.png"
     }
 ]
 
 export const silverMedals = [
     {
         sport: "Women's 10+10km Skiathlon",
-        medalist: ["Ebba Andersson"]
+        medalist: ["Ebba Andersson"],
+        image: "./images/medalists/ebba-andersson.png"
     },
     {
         sport: "Women's Sprint Classic",
-        medalist: ["Jonna Sundling"]
+        medalist: ["Jonna Sundling"],
+        image: "./images/medalists/jonna-sundling.png"
     },
     {
         sport: "Women's 10km Interval Start Freestyle",
-        medalist: ["Ebba Andersson"]
+        medalist: ["Ebba Andersson"],
+        image: "./images/medalists/ebba-andersson2.png"
     },
     {
         sport: "Women's 4x7.5km Relay",
-        medalist: ["Linn Svahn", "Ebba Andersson", "Frida Karlsson", "Jonna Sundling"]
+        medalist: ["Linn Svahn", "Ebba Andersson", "Frida Karlsson", "Jonna Sundling"],
+        image: "./images/medalists/linn-ebba-frida-jonna.png"
     },
     {
         sport: "Women's Giant Slalom",
-        medalist: ["Sara Hector"]
+        medalist: ["Sara Hector"],
+        image: "./images/medalists/sara-hector.png"
     },
     {
         sport: "Women's Biathlon 4 x 6km Relay",
-        medalist: ["Linn Gestblom","Anna Magnusson", "Elvira Öberg", "Hanna Öberg"]
+        medalist: ["Linn Gestblom", "Anna Magnusson", "Elvira Öberg", "Hanna Öberg"],
+        image: "./images/medalists/elvira-anna-hanna-linn.png"
     }
 ]
 
 export const bronzeMedals = [
     {
         sport: "Cross-Country Skiing - Women's Sprint Classic",
-        medalist: ["Maja Dahlqvist"]
+        medalist: ["Maja Dahlqvist"],
+        image: "./images/medalists/maja-dahlqvist.png"
     },
     {
         sport: "Alpine Skiing - Women's Slalom",
-        medalist: ["Anna Swenn-Larsson"]
+        medalist: ["Anna Swenn-Larsson"],
+        image: "./images/medalists/anna-swenn-larsson.png"
     },
     {
         sport: "Freestyle Skiing - Women's Ski Cross",
-        medalist: ["Sandra Näslund"]
+        medalist: ["Sandra Näslund"],
+        image: "./images/medalists/sandra-naslund.png"
     },
     {
         sport: "Biathlon - Men's 4x7.5km Relay",
-        medalist: ["Viktor Brandt", "Jesper Nelin", "Martin Ponsiluoma", "Sebastian Samuelsson"]
+        medalist: ["Viktor Brandt", "Jesper Nelin", "Martin Ponsiluoma", "Sebastian Samuelsson"],
+        image: "./images/medalists/viktor-jesper-martin-sebastian.png"
     }
 ]
